b9166e96079c49e25913b258533e33ea627f3b7e
Chad Masso <chad.m.masso@gmail.com>
2012-10-19 12:00:38 -0500

    made version configurable

--- src/indeed.php
+++ src/indeed.php
@@ -2,7 +2,6 @@
 
 class Indeed{
 
-    const API_VERSION = "2";
     const DEFAULT_FORMAT = "json";
     const API_SEARCH_ENDPOINT = "http://api.indeed.com/ads/apisearch";
     const API_JOBS_ENDPOINT = "http://api.indeed.com/ads/apigetjobs";
@@ -10,8 +9,9 @@ class Indeed{
     private static $API_SEARCH_REQUIRED = array("userip", "useragent", array("q", "l"));
     private static $API_JOBS_REQUIRED = array("jobkeys");
 
-    public function __construct($publisher){
+    public function __construct($publisher, $version = "2"){
         $this->publisher = $publisher;
+        $this->version = $version;
     }
 
     public function search($args){
@@ -28,7 +28,7 @@ class Indeed{
         $format = (array_key_exists("format", $args) ? $args["format"] : self::DEFAULT_FORMAT);
         $raw = ($format == "xml" ? true : (array_key_exists("raw", $args) ? $args["raw"] : false));
         $args["publisher"] = $this->publisher;
-        $args["v"] = self::API_VERSION;
+        $args["v"] = $this->version;
         $args["format"] = $format;
         $c = curl_init(sprintf("%s?%s", $endpoint, http_build_query($args)));
         curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
